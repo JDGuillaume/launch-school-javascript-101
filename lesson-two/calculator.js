9c09fe1ecae30d11185d18da4289d82cb06457f8
Joey <josephdguillaume@gmail.com>
2020-12-23 07:45:24 -0500

    Refactored Calculator.js to validate input

--- lesson-two/calculator.js
+++ lesson-two/calculator.js
@@ -1,33 +1,65 @@
+// Attach the readline library to the readline variable for ease of use.
 const readline = require('readline-sync');
 
+// Create a reusable function that prepends log statements with the => marker.
+function prompt(message) {
+  console.log(`=> ${message}`);
+}
+
+// Create a function to determine whether the users input is valid.
+function invalidNumber(number) {
+  return number.trimStart() === '' || Number.isNaN(Number(number));
+}
+
 // Welcome the user to the program.
-console.log('Welcome to Calculator!');
+prompt('Welcome to Calculator!');
 
-// Ask the user for the first number.
-console.log("What's the first number?");
-const firstNumber = Number(readline.question());
+// Ask the user for the first number and validate their input.
+prompt("What's the first number?");
+let firstNumber = readline.question();
 
-// Ask the user for the second number.
-console.log("What's the second number?");
-const secondNumber = Number(readline.question());
+while (invalidNumber(firstNumber)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  firstNumber = readline.question();
+}
 
-// Ask the user for an operation to perform.
-console.log(
+// Ask the user for the second number and validate their input.
+prompt("What's the second number?");
+let secondNumber = readline.question();
+
+while (invalidNumber(secondNumber)) {
+  prompt("Hmm... that doesn't look like a valid number.");
+  secondNumber = readline.question();
+}
+
+// Ask the user for an operation to perform and validate their input.
+prompt(
   'What operation would you like to perform?\n1) Add 2) Subtract 3) Multiply 4) Divide'
 );
-const operation = readline.question();
+let operation = readline.question();
+
+while (!['1', '2', '3', '4'].includes(operation)) {
+  prompt('Must choose 1, 2, 3 or 4');
+  operation = readline.question();
+}
 
 // Perform the operation on the two numbers.
 let output;
-if (operation === '1') {
-  output = firstNumber + secondNumber;
-} else if (operation === '2') {
-  output = firstNumber - secondNumber;
-} else if (operation === '3') {
-  output = firstNumber * secondNumber;
-} else {
-  output = firstNumber / secondNumber;
+
+switch (operation) {
+  case '1':
+    output = Number(firstNumber) + Number(secondNumber);
+    break;
+  case '2':
+    output = Number(firstNumber) - Number(secondNumber);
+    break;
+  case '3':
+    output = Number(firstNumber) * Number(secondNumber);
+    break;
+  case '4':
+    output = Number(firstNumber) / Number(secondNumber);
+    break;
 }
 
 // Print the result to the terminal.
-console.log(`The result is: ${output}`);
+prompt(`The result is: ${output}`);
