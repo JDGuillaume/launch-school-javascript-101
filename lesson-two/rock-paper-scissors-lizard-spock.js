de8809990bd40109faef8594690b0aaf38bc5597
Joey <josephdguillaume@gmail.com>
2020-12-27 07:41:37 -0500

    Updated RPS Bonus to Best of 5

--- lesson-two/rock-paper-scissors-lizard-spock.js
+++ lesson-two/rock-paper-scissors-lizard-spock.js
@@ -9,9 +9,10 @@ const WIN_CONDITIONS = {
 };
 const VALID_CHOICES = Object.keys(WIN_CONDITIONS);
 
-let continuePlaying = true;
-
 const abbreviatedChoices = {};
+
+let playerWinCount = 0;
+let computerWinCount = 0;
 
 function shortenChoices(VALID_CHOICES) {
   VALID_CHOICES.forEach(choice => {
@@ -28,6 +29,11 @@ function prompt(message) {
   console.log(`=> ${message}`);
 }
 
+function displayChoices(choice, computerChoice) {
+  console.log('');
+  prompt(`You chose ${choice}, computer chose ${computerChoice}.`);
+}
+
 function findWinner(choice, computerChoice) {
   if (choice === computerChoice) {
     return 'tie';
@@ -38,25 +44,40 @@ function findWinner(choice, computerChoice) {
   }
 }
 
-function displayChoices(choice, computerChoice) {
-  prompt(`You chose ${choice}, computer chose ${computerChoice}.`);
-}
-
 function displayWinner(winner) {
   switch (winner) {
     case 'tie':
-      prompt("It's a tie!");
+      prompt("It's a tie!\n");
       break;
     case 'player':
-      prompt('You win!');
+      prompt('You win!\n');
       break;
     case 'computer':
-      prompt('Computer wins!');
+      prompt('Computer wins!\n');
       break;
   }
 }
 
-while (continuePlaying) {
+function updateWinCount(winner) {
+  switch (winner) {
+    case 'player':
+      playerWinCount += 1;
+      break;
+    case 'computer':
+      computerWinCount += 1;
+      break;
+  }
+}
+
+function displayOverallWinner(playerWinCount) {
+  if (playerWinCount === 5) {
+    prompt('You won best of 5!');
+  } else {
+    prompt('The computer won best of 5!');
+  }
+}
+
+while (playerWinCount !== 5 && computerWinCount !== 5) {
   prompt(
     `Choose one: ${Object.entries(abbreviatedChoices)
       .join(' | ')
@@ -74,18 +95,11 @@ while (continuePlaying) {
   let randomIndex = Math.floor(Math.random() * VALID_CHOICES.length);
   let computerChoice = VALID_CHOICES[randomIndex];
 
-  let winner = findWinner(choice, computerChoice);
   displayChoices(choice, computerChoice);
+  let winner = findWinner(choice, computerChoice);
   displayWinner(winner);
-
-  prompt('Do you want to play again (y/n)?');
-  let answer = readline.question();
-  while (answer[0] !== 'n' && answer[0] !== 'y') {
-    prompt('Please enter "y" or "n".');
-    answer = readline.question().toLowerCase();
-  }
-
-  if (answer !== 'y') {
-    continuePlaying = false;
-  }
+  updateWinCount(winner);
+  console.log(`Player: ${playerWinCount}, Computer: ${computerWinCount}\n`);
 }
+
+displayOverallWinner(playerWinCount);
