2231e1dc4d8cd6bc4789374bed96b745d3a0b279
Joey <josephdguillaume@gmail.com>
2020-12-29 08:53:29 -0500

    Updated formatting

--- lesson-three/easy-one.js
+++ lesson-three/easy-one.js
@@ -1,13 +1,3 @@
-// Q1: Will the code below raise an error?
-let numbers = [1, 2, 3];
-numbers[6] = 5;
-
-// Q1 -- Bonus
-
-let bonusNumbers = [1, 2, 3];
-bonusNumbers[6] = 5;
-bonusNumbers[4]; // what will this line return?
-
 /* A1: No, the above code will not raise an error.
        JavaScript will place the value 5 at index 6.
        If you attempt to access index 4 or 5, you will get undefined.
@@ -15,19 +5,19 @@ bonusNumbers[4]; // what will this line return?
 
 // ----------------------------------------------------------------------
 
-/* Q2: How can you determine whether a given string
-       ends with an exclamation mark (!)? */
-let str1 = 'Come over here!'; // true
-let str2 = "What's up, Doc?"; // false
-
 /* A2: We can use a regular expression
        to check if the string ends with an exclamation mark! */
+
+let str1 = 'Come over here!'; // true
+let str2 = "What's up, Doc?";
 
 function endsWithExclamation(string) {
   let regex = RegExp(/!$/);
   return regex.test(string);
 }
+
 console.log();
+
 console.log(
   `Does "${str1}" end with an exclamation mark? -- ${endsWithExclamation(str1)}`
 );
@@ -39,25 +29,19 @@ console.log(
 
 // ----------------------------------------------------------------------
 
-/* Q3: Determine whether the following object of people
-       and their age contains an entry for 'Spot': */
-
+// A3:
 let ages = {Herman: 32, Lily: 30, Grandpa: 402, Eddie: 10};
 
-// A3:
 console.log(
   `ages contains an entry for 'Spot' -- ${Object.keys(ages).includes('Spot')}\n`
 );
 
 // ----------------------------------------------------------------------
 
-/* Q4: Using the following string, create a new string that contains all
-       lowercase letters except for the first character,
-       which should be capitalized. */
+// A4:
 
 let munstersDescription = 'the Munsters are CREEPY and Spooky.';
 
-// A4:
 function sentenceCase(string) {
   return string[0].toUpperCase() + string.slice(1).toLowerCase();
 }
@@ -67,21 +51,16 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-// Q5: What will the following code output?
-console.log(false == '0');
-console.log(false === '0');
-
-// A5: true, false
+// A5:
+console.log(false == '0'); // true
+console.log(false === '0'); // false
 
 // ----------------------------------------------------------------------
 
-// Q6: We have most of the Munster family in our ages object:
-// Add entries for Marilyn and Spot to the object:
-
+// A6:
 let munsterAges = {Herman: 32, Lily: 30, Grandpa: 5843, Eddie: 10};
 let additionalAges = {Marilyn: 22, Spot: 237};
 
-// A6:
 console.log();
 Object.assign(munsterAges, additionalAges);
 console.log(munsterAges);
@@ -89,7 +68,7 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-// Q7: Determine whether the name Dino appears in the strings below.
+// A7:
 let dinoStr1 =
   'Few things in life are as important as house training your pet dinosaur.';
 let dinoStr2 = 'Fred and Wilma have a pet dinosaur named Dino.';
@@ -109,30 +88,25 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-// Q8: How can we add the family pet, "Dino", to the following array?
+// A8:
 let flintstones = ['Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles'];
 
-// A8:
 flintstones.push('Dino');
 console.log(flintstones);
 console.log();
 
 // ----------------------------------------------------------------------
 
-// Q9: How can we add multiple items to our array? ('Dino' and 'Hoppy')
-let flinstones2 = ['Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles'];
-
 // A9:
+let flinstones2 = ['Fred', 'Barney', 'Wilma', 'Betty', 'Bambam', 'Pebbles'];
 flinstones2.push('Dino', 'Happy');
 console.log(flinstones2);
 console.log();
 
 // ----------------------------------------------------------------------
 
-/* Q10: Return a new version of this sentence that ends
-        just before the word house. Don't worry about spaces or punctuation:
-        remove everything starting from the beginning of house to the end of
-        the sentence. */
+// A10:
+
 let advice =
   'Few things in life are as important as house training your pet dinosaur.';
 
--- lesson-three/easy-two.js
+++ lesson-three/easy-two.js
@@ -1,35 +1,12 @@
-/* Q1: Given a string, return a new string that replaces
-       every occurrence of the word "important" with "urgent": */
-
+// A1:
 let advice =
   'Few things in life are as important as house training your pet dinosaur.';
 
-// A1:
 console.log(advice.replaceAll('important', 'urgent'));
 
 // ----------------------------------------------------------------------
 
-/* Q2:
-  The Array.prototype.reverse method reverses the order of elements in an array,
-  and Array.prototype.sort can rearrange the elements in a variety of ways,
-  including descending. Both of these methods mutate the original array as shown
-  below. Write two distinct ways of reversing the array without mutating the
-  original array. Use reverse for the first solution, and sort for the second.
-
-*/
-
-let numbers = [1, 2, 3, 4, 5];
-numbers.reverse();
-console.log(numbers); // [ 5, 4, 3, 2, 1 ]
-
-numbers = [1, 2, 3, 4, 5];
-numbers.sort((num1, num2) => num2 - num1);
-console.log(numbers); // [ 5, 4, 3, 2, 1 ]
-
-// Q2 Bonus: Can you do it using the Array.prototype.forEach() method?
-
 // A2:
-
 [...numbers].reverse();
 [...numbers].sort((num1, num2) => num2 - num1);
 
@@ -46,17 +23,13 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-/*
-  Q3: Given a number and an array,
-  determine whether the number is included in the array.
-*/
+// A3:
 
 let numbersArray = [1, 2, 3, 4, 5, 15, 16, 17, 95, 96, 99];
 
 let number1 = 8; // false
 let number2 = 95; // true
 
-// A3:
 console.log(numbersArray.includes(number1));
 console.log(numbersArray.includes(number2));
 
@@ -64,13 +37,8 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-// Q4: Starting with the string:
-
-let famousWords = 'seven years ago...';
-
-// show two different ways to put the expected "Four score and " in front of it.
-
 // A4:
+let famousWords = 'seven years ago...';
 
 // Option 1:
 console.log(`Four score and ${famousWords}`);
@@ -83,11 +51,6 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-/*
-  Q5: Given an array of numbers [1, 2, 3, 4, 5], mutate the array by removing
-  the number at index 2, so that the array becomes [1, 2, 4, 5].
-*/
-
 // A5:
 let arrayToMutate = [1, 2, 3, 4, 5];
 arrayToMutate.splice(2, 1);
@@ -98,26 +61,17 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-// Q6: Suppose we build an array like this:
-
+// A6:
 let flintstones = ['Fred', 'Wilma'];
 flintstones.push(['Barney', 'Betty']);
 flintstones.push(['Bambam', 'Pebbles']);
-
-// This code will create a nested array that looks like this:
-// ['Fred', 'Wilma', ['Barney', 'Betty'], ['Bambam', 'Pebbles']];
-
-/* Create a new array that contains all of the above values,
-   but in an un-nested format: */
-
-// A6:
 console.log(flintstones.flat(1));
 
 console.log();
 
 // ----------------------------------------------------------------------
 
-// Q7: Consider the following object:
+// A7:
 let newFlintstones = {
   Fred: 0,
   Wilma: 1,
@@ -127,10 +81,6 @@ let newFlintstones = {
   Pebbles: 5,
 };
 
-/* Create an array from this object that contains only two elements:
-   Barney's name and Barney's number: */
-
-// A7:
 let barneyArray = Object.entries(newFlintstones)[2];
 console.log(barneyArray);
 
@@ -138,8 +88,7 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-/* Q8: How would you check whether the objects assigned to
-   variables numbers and table below are arrays? */
+// A8:
 
 let newNumbers = [1, 2, 3, 4]; // true
 let table = {field1: 1, field2: 2, field3: 3, field4: 4}; // false
@@ -151,14 +100,9 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-/* Q9: Back in the stone age (before CSS), we used spaces to align things
-       on the screen. If we have a 40-character wide table of Flintstone
-       family members, how can we center the following title above
-       the table with spaces? */
+// A9:
 
 let title = 'Flintstone Family Members';
-
-// A9:
 
 function calculatePadding(string) {
   let lineLength = 40;
@@ -180,13 +124,11 @@ console.log();
 
 // ----------------------------------------------------------------------
 
-/* Q10: Write two one-line expressions to count the number of
-   lower-case t characters in each of the following strings: */
+// A10:
 
 let statement1 = 'The Flintstones Rock!';
 let statement2 = 'Easy come, easy go.';
 
-// A10:
 function countTinyTs(string) {
   return [...string.matchAll('t')].length;
 }
