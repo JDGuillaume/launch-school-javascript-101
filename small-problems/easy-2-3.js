cc04287e54d47be4f0ff48f484711a9971362813
Joey <josephdguillaume@gmail.com>
2021-01-10 13:26:53 -0500

    Completed Easy 2-3

---
+++ small-problems/easy-2-3.js
@@ -0,0 +1,22 @@
+// Understand the Problem
+// Input: Integer(s)
+// Output: Integer
+
+// The function should:
+// Take two integers as arguments.
+// Return the product of those two integers.
+
+// Examples / Test Cases
+console.log(multiply(5, 3) === 15); // logs true
+
+// Data Structure
+// The function should rely on a return statement and an algebraic expression
+
+// Algorithm
+// Create a function that accepts two integers.
+// Return the expression integer1 * integer2
+
+// Code
+function multiply(number1, number2) {
+  return number1 * number2;
+}
